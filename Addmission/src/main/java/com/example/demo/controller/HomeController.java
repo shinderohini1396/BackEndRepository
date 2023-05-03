d8157f60f7ea0731ef54e974981e69be91130a77
shinderohini1396 <shinderohini@1396>
2023-05-03 13:16:51 +0530

    Addmission Open

---
+++ Addmission/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@PostMapping(value = "/insertdata")
+	public String saveData()
+	{
+		return "Addmission Open";
+	}
+}
